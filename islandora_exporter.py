01e9d0541604692a49f3600a03ea5f002d086275
Mark Jordan <mjordan@sfu.ca>
2020-12-09 20:29:13 -0800

    Some fixups.

--- islandora_exporter.py
+++ islandora_exporter.py
@@ -7,6 +7,7 @@ import json
 import mimetypes
 
 mimetypes.init()
+collection_pid = sys.argv[4]
 output_directory = sys.argv[3]
 metadata_datastream = sys.argv[2]
 site_base_url = sys.argv[1]
@@ -21,7 +22,7 @@ def fetch_children(collection_pid, parent_directory):
     children = children_response_body_dict['response']['docs']
 
     for child in children:
-        if child['RELS_EXT_hasModel_uri_t'] = 'info:fedora/islandora:collectionCModel':
+        if child['RELS_EXT_hasModel_uri_t'] == 'info:fedora/islandora:collectionCModel':
             # need to check for duplicates here
             new_parent = os.path.join(parent_directory, child['fgs_label_t'])
             os.makedirs(new_parent)
@@ -29,21 +30,23 @@ def fetch_children(collection_pid, parent_directory):
 
         else:
             # this won't work with compound objects currently
-            obj_url = site_base_url + 'islandora/object/' + child['PID'] + '/datastream/OBJ/download'
+            obj_url = site_base_url + '/islandora/object/' + child['PID'] + '/datastream/OBJ/download'
+            print("Downloading " +  site_base_url + "/islandora/object/" + child['PID'])
             obj = requests.get(obj_url)
             mimetype = obj.headers["content-type"]
             extension = mimetypes.guess_extension(mimetype)
-            obj_content_path = os.path.join(parent_directory, (child['fgs_label_t'] + extension))
+            obj_content_path = os.path.join(output_directory, (child['fgs_label_t'] + extension))
+            print(obj_content_path)
             with open(obj_content_path, 'wb') as file:
                 file.write(obj.content)
 
-        print("Downloading " +  site_base_url + "islandora/object/" + object['PID'])
-
-        metadata_url = site_base_url + 'islandora/object/' + child['PID'] + '/datastream/' + metadata_datastream + '/download'
+        metadata_query = 'solr/PID:' + child['PID'].replace(':', '%3A')
+        metadata_url = site_rest_url + 'solr/PID:' + metadata_query
+        metadata_url = site_base_url + '/islandora/object/' + child['PID'] + '/datastream/' + metadata_datastream + '/download'
         metadata_xml = urllib.request.urlopen(metadata_url).read().decode('utf-8')
         metadata_xml_path = os.path.join(parent_directory, (child['fgs_label_t'] + ".xml"))
         with open(metadata_xml_path, 'w', encoding="utf-8") as file:
             file.write(metadata_xml)
 
-if __name__ == "__main__"
-    fetch_children("islandora%3Aroot", output_directory)
+if __name__ == "__main__":
+    fetch_children(collection_pid, output_directory)
